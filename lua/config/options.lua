fb9d176f658e618ce226b0ed7befa324918ca7ad
Michael Savin <jtprogru@gmail.com>
2024-10-29 18:46:08 +0300

    make some configurations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local HOME = os.getenv("HOME")
+
+vim.g.python3_host_prog = HOME .. "/.config/nvim/venv/bin/python3"
 
 opt.relativenumber = false -- Relative line numbers
+
+vim.filetype.add {
+  extension = {
+    gotmpl = "gotmpl",
+  },
+  pattern = {
+    [".*/templates/.*%.gotmpl"] = "helm",
+    [".*/.*%.gotmpl"] = "helm",
+    [".*/templates/.*%.ya?ml"] = "helm",
+    ["helmfile.*%.ya?ml"] = "helm",
+  },
+}
