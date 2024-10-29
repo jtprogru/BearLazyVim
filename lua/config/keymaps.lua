fee85259d15570483e941af564749e730d59f8b9
Michael Savin <jtprogru@gmail.com>
2024-10-29 17:25:01 +0300

    Customize keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- This file is automatically loaded by lazyvim.config.init
+-- Default keymaps that are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+local map = vim.keymap.set
+
+-- Remap for dealing with buffers
+map("n", "<tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<S-tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+
+-- commenting
+map("n", "<leader>/", "<cmd>normal gcc<cr>", { desc = "Comment current line" })
