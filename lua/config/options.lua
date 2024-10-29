46cc9a10f0d0c42819a3ecd1a27da7388676b01d
Michael Savin <jtprogru@gmail.com>
2024-10-29 19:24:39 +0300

    Migrate to ruff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-local HOME = os.getenv("HOME")
-
-vim.g.python3_host_prog = HOME .. "/.config/nvim/venv/bin/python3"
+vim.g.lazyvim_python_lsp = "ruff"
 
 opt.relativenumber = false -- Relative line numbers
 
