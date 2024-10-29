d3b6ea3ddc95bd06757a8d4a680efce67e7b7cf3
Michael Savin <jtprogru@gmail.com>
2024-10-29 20:24:01 +0300

    Configure Python3 for LazyVim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local HOME = os.getenv("HOME")
+vim.g.python3_host_prog = HOME .. "/.config/nvim/venv/bin/python3"
+
 vim.g.lazyvim_python_lsp = "ruff"
 
+
 opt.relativenumber = false -- Relative line numbers
+
+vim.g.bigfile_size = 1024 * 1024 * 1024 * 1.5 -- 1.5 GB
 
 vim.filetype.add {
   extension = {
