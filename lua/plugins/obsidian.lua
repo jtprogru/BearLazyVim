475e5cd46adb4ec77382443c17f7808dc0ca6697
Michael Savin <jtprogru@gmail.com>
2024-10-31 21:16:04 +0300

    Migrate from obsidian-bridge.nvim to obsidian.nvim

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,55 +1,42 @@
 return {
-  "oflisback/obsidian-bridge.nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    require("obsidian-bridge").setup({
-      obsidian_server_address = "http://localhost:27123",
-      scroll_sync = true,
-    })
-  end,
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   event = {
-    "BufReadPre *.md",
-    "BufNewFile *.md",
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    -- refer to `:h file-pattern` for more examples
+    "BufReadPre "
+      .. vim.fn.expand("~")
+      .. "Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE/**/*.md",
+    "BufNewFile " .. vim.fn.expand("~") .. "Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE/**/*.md",
   },
-  lazy = false,
-}
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
 
--- return {
---   "epwalsh/obsidian.nvim",
---   version = "*", -- recommended, use latest release instead of latest commit
---   lazy = true,
---   ft = "markdown",
---   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
---   event = {
---     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
---     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
---     -- refer to `:h file-pattern` for more examples
---     "BufReadPre "
---       .. vim.fn.expand("~")
---       .. "Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE/*.md",
---     "BufNewFile " .. vim.fn.expand("~") .. "Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE/*.md",
---   },
---   dependencies = {
---     -- Required.
---     "nvim-lua/plenary.nvim",
---
---     -- see below for full list of optional dependencies 👇
---   },
---   opts = {
---     workspaces = {
---       {
---         name = "personal",
---         path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE",
---       },
---       {
---         name = "test",
---         path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/ZTK",
---       },
---     },
---
---     -- see below for full list of options 👇
---   },
--- }
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE",
+      },
+      {
+        name = "test",
+        path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/ZTK",
+      },
+    },
+
+    -- see below for full list of options 👇
+    notes_subdir = "00-Inbox",
+    templates = {
+      folder = "Templates",
+      date_format = "%YYYY-%mm-%dd",
+      time_format = "%HH:%MM:%ss",
+    },
+  },
+}
