50f930f0db1a74d48e7513e09f1c5a5d100edef4
Michael Savin <jtprogru@gmail.com>
2024-11-01 21:02:50 +0300

    Update config for ubsidian plugin

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,42 +1,96 @@
+local random = math.random
+local function custom_uuid4()
+  local template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+  return string.gsub(template, "[xy]", function(c)
+    local v = (c == "x") and random(0, 0xf) or random(8, 0xb)
+    return string.format("%x", v)
+  end)
+end
+
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
+  lazy = false,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   event = {
     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
     -- refer to `:h file-pattern` for more examples
-    "BufReadPre "
-      .. vim.fn.expand("~")
-      .. "Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE/**/*.md",
-    "BufNewFile " .. vim.fn.expand("~") .. "Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE/**/*.md",
+    "BufReadPre *.md",
+    "BufNewFile *.md",
   },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-treesitter/nvim-treesitter",
   },
   opts = {
+    wiki_link_func = function(opts)
+      return require("obsidian.util").use_alias_only(opts)
+    end,
+    -- Either 'wiki' or 'markdown'.
+    preferred_link_style = "wiki",
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        return custom_uuid4()
+      else
+        return title
+      end
+    end,
     workspaces = {
       {
         name = "personal",
         path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE",
+        overrides = {
+          notes_subdir = "00-Inbox",
+          daily_notes = {
+            -- Optional, if you keep daily notes in a separate directory.
+            folder = "Journal/",
+            -- Optional, if you want to change the date format for the ID of daily notes.
+            date_format = "%Y/%m/%Y-%m-%d",
+            -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+            template = "Input/Daily template.md",
+          },
+        },
       },
       {
         name = "test",
         path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/ZTK",
+        overrides = {
+          notes_subdir = "Inbox",
+          daily_notes = {
+            -- Optional, if you keep daily notes in a separate directory.
+            folder = "Journal/",
+            -- Optional, if you want to change the date format for the ID of daily notes.
+            date_format = "%Y/%m/%Y-%m-%d",
+            -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+            template = "Daily template.md",
+          },
+        },
       },
     },
 
-    -- see below for full list of options 👇
-    notes_subdir = "00-Inbox",
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 4,
+    },
     templates = {
       folder = "Templates",
-      date_format = "%YYYY-%mm-%dd",
+      date_format = "%Y-%m-%d",
       time_format = "%HH:%MM:%ss",
+    },
+    attachments = {
+      img_folder = "Static",
     },
   },
 }
