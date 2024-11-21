8b04cd40fbf4e76d6228227bd2d9725e83d4d22d
Michael Savin <jtprogru@gmail.com>
2024-11-21 23:51:49 +0300

    fix: Clean keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,9 +4,9 @@
 local map = vim.keymap.set
 
 -- Remap for dealing with buffers
-map("n", "<tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<tab>", "<cmd>bnext<cr>", { desc = "Switch to next buffer" })
+map("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Switch to prev buffer" })
+map("n", "<leader>x", LazyVim.ui.bufremove, { desc = "Delete current buffer" })
 
--- commenting
--- map("n", "<leader>/", "<cmd>normal gcc<cr>", { desc = "Comment current line" })
-map("n", "<leader>x", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
+-- Work with GitSigns
+-- map("n", "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", { desc = "Next Hunk" })
