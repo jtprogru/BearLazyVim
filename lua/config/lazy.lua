1e818772c3d11845b9b859ffd83ce15788f84a39
Michael Savin <jtprogru@gmail.com>
2024-12-19 12:53:53 +0300

    upd: Migrate from Codeium to GH Copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.ai.codeium" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.lang.ansible" },
