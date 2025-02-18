10282bafdb9a433e3b8fe3c4ea59dbb7ca751caf
Michael Savin <jtprogru@gmail.com>
2025-02-18 19:08:33 +0300

    Add obsidian support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,8 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.ai.copilot" },
-    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+    -- { import = "lazyvim.plugins.extras.ai.copilot" },
+    -- { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.coding.blink" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,20 @@
+-- plugins for notetaking and knowledge management
+local obsidian_config = require("config.obsidian")
+
+return {
+  "epwalsh/obsidian.nvim",
+  verion = "*",
+  lazy = false,
+  ft = "markdown",
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    "BufReadPre " .. vim.fn.expand("~/Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE/**/*.md"),
+    "BufNewFile " .. vim.fn.expand("~/Library/Mobile Documents/iCloud~md~obsidian/Documents/SPACE/**/*.md"),
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  opts = obsidian_config,
+}
