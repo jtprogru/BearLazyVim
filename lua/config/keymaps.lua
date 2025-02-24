36dbe24138680c36c779d4e3d8368a0aec9fade2
Michael Savin <jtprogru@gmail.com>
2025-02-24 12:36:10 +0300

    Disable HARDMODE

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,25 +10,25 @@ map("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Switch to prev buffer" })
 -- Work with GitSigns
 map("n", "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", { desc = "Next Hunk" })
 
--- VIM hard mode (disable backspace and arrows)
--- Source: https://www.mailslurp.com/blog/vim-hard-mode/
-local hardmode = true
-if hardmode then
-  -- Show an error message if a disabled key is pressed
-  local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
-
-  -- Disable arrow keys in insert mode with a styled message
-  -- map("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
-  -- map("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
-  -- map("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
-  -- map("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
-  -- map("i", "<Del>", "<C-o>" .. msg, { noremap = true, silent = false })
-  -- map("i", "<BS>", "<C-o>" .. msg, { noremap = true, silent = false })
-
-  -- Disable arrow keys in normal mode with a styled message
-  map("n", "<Up>", msg, { noremap = true, silent = false })
-  map("n", "<Down>", msg, { noremap = true, silent = false })
-  map("n", "<Left>", msg, { noremap = true, silent = false })
-  map("n", "<Right>", msg, { noremap = true, silent = false })
-  -- map("n", "<BS>", msg, { noremap = true, silent = false })
-end
+-- -- VIM hard mode (disable backspace and arrows)
+-- -- Source: https://www.mailslurp.com/blog/vim-hard-mode/
+-- local hardmode = true
+-- if hardmode then
+--   -- Show an error message if a disabled key is pressed
+--   local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
+--
+--   -- Disable arrow keys in insert mode with a styled message
+--   -- map("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
+--   -- map("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
+--   -- map("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
+--   -- map("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
+--   -- map("i", "<Del>", "<C-o>" .. msg, { noremap = true, silent = false })
+--   -- map("i", "<BS>", "<C-o>" .. msg, { noremap = true, silent = false })
+--
+--   -- Disable arrow keys in normal mode with a styled message
+--   map("n", "<Up>", msg, { noremap = true, silent = false })
+--   map("n", "<Down>", msg, { noremap = true, silent = false })
+--   map("n", "<Left>", msg, { noremap = true, silent = false })
+--   map("n", "<Right>", msg, { noremap = true, silent = false })
+--   -- map("n", "<BS>", msg, { noremap = true, silent = false })
+-- end
